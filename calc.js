2b7b21221dd4e46460264b08324408efb2c0cc96
sinovuyo <sinovuyo@muluma.co.za>
2024-06-19 12:43:23 +0200

    Added my project

---
+++ calc.js
@@ -0,0 +1,40 @@
+const add = ()=>{
+
+let input1;
+let input2;
+input1 = document.querySelector("#input1").value
+input2 = document.querySelector("#input2").value
+
+let result =  +input1 + + input2
+//alert ("Result:" + result);
+console.log("Results", result)
+
+document.querySelector("#resultValue").innerHTML = result
+
+}
+const minus = ()=>{
+
+    let input1;
+    let input2;
+    input1 = document.querySelector("#input1").value
+    input2 = document.querySelector("#input2").value
+}
+
+const multiple = ()=>{
+
+    let input1;
+    let input2;
+    input1 = document.querySelector("#input1").value
+    input2 = document.querySelector("#input2").value
+}
+
+const divide = ()=>{
+
+    let input1;
+    let input2;
+    input1 = document.querySelector("#input1").value
+    input2 = document.querySelector("#input2").value
+}
+
+
+
